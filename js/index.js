6e1420c15f08ae83468b089b04494e7e4dd0f766
sanix-darker <s4nixd@gmail.com>
2020-10-09 13:16:31 +0100

    Refactoring...

--- js/index.js
+++ js/index.js
@@ -1,53 +1,51 @@
-
+/**
+ * This method will send the request for a Looking or a found
+ * @param {*} type 
+ */
 var send = function (type){
-
   document.querySelector(".action").innerHTML = (LangWanted == "fr") ? "Envoies en cours..." : "Sending....";
 
-   var formData = new FormData();
+  var formData = new FormData();
+  formData.append("file", (type == "looking") ? input.files[0]: input2.files[0]);
 
-   if (type == "looking")
-      formData.append("file", input.files[0]);
-   else
-      formData.append("file", input2.files[0]);
-
-   var request = new XMLHttpRequest();
-   request.open("POST", "http://");
-   request.onload = function () {
+  var request = new XMLHttpRequest();
+  request.open("POST", "http://");
+  request.onload = function () {
      
-     var response = JSON.parse(request.responseText);
-     if (request.readyState == 4 && request.status == "200") {
+    var response = JSON.parse(request.responseText);
+    if (request.readyState == 4 && request.status == "200") {
 
-      const {
-        image,
-        categorie,
-        adresse,
-        lat, lng,
-        description
-      } = getParamsFromType(type);
+    const {
+      image,
+      categorie,
+      adresse,
+      lat, lng,
+      description
+    } = getParamsFromType(type);
 
-      var request2 = new XMLHttpRequest();
-      request2.open("GET", 
-      API_URL + "/api/lost?method=post&image=" + image + 
-      "&categorie=" + categorie + 
-      "&adresse=" + adresse + 
-      "&lat=" + lat + "&lng=" + lng +
-      "&description=" + description + 
-      "&type="+type
-      );
-      request2.onload = function () {
-        
-        var response2 = JSON.parse(request2.responseText);
-        if (request2.readyState == 4 && request2.status == "200") {
-          document.querySelector('.close').click();
-          // close
-          initMap();
-          // Center sur les parametres voulus
-          moveToLocation(parseFloat(lat), parseFloat(lng));
-          
-          resetForm();
-        }
+    var request2 = new XMLHttpRequest();
+    request2.open("GET", 
+    API_URL + "/api/lost?method=post&image=" + image + 
+    "&categorie=" + categorie + 
+    "&adresse=" + adresse + 
+    "&lat=" + lat + "&lng=" + lng +
+    "&description=" + description + 
+    "&type="+type
+    );
+    request2.onload = function () {
+      
+      var response2 = JSON.parse(request2.responseText);
+      if (request2.readyState == 4 && request2.status == "200") {
+        document.querySelector('.close').click();
+        // close
+        initMap();
+        // Center sur les parametres voulus
+        moveToLocation(parseFloat(lat), parseFloat(lng));
+        // we reset the form
+        resetForm();
       }
-      request2.send(null);
+    }
+    request2.send(null);
     }
   }
   request.send(formData);
