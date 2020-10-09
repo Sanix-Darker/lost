fcb77d052931aa34f9415d85a8276f5f49d5d7cb
sanix-darker <s4nixd@gmail.com>
2020-10-09 13:02:19 +0100

    Refactoring... on image preview

--- js/forms.js
+++ js/forms.js
@@ -1,3 +1,7 @@
+/***
+ * forms.js
+ * this file is for all form stuffs
+ */
 
 // To manage Filter on the level of the thing lost
 var filter = function(level){
@@ -6,7 +10,7 @@ var filter = function(level){
     document.getElementById("filter_3").classList.remove("menu_selected");
     document.getElementById("filter_4").classList.remove("menu_selected");
     document.getElementById("filter_"+level).classList.add("menu_selected");
-  
+
     if(parseInt(level) === 4){
       locations = loc;
     }else{
@@ -14,50 +18,40 @@ var filter = function(level){
     }
     generateMapFromLocations(locations);
     locations = loc;
-  }
-  
-  
-  // Formulaires
-  var input = document.getElementById('previewfile1');
-  var img = document.getElementById('previewimg1');
-  
-  input.addEventListener('change', function(event){
-     var file = event.target.files[0];
-  
-     if(file.size > 500000){
+}
+
+
+/**
+ * We preview the img
+ * @param {*} event 
+ * @param {*} Id 
+ */
+var previewImg = function (event, Id){
+    var file = event.target.files[0];
+
+    if(file.size > 500000){
        alert("Your image is too hight!")
        return false;
-     }
-  
-     if (file) {
+    }
+
+    if (file) {
         var fileReader = new FileReader();
-  
+
         fileReader.addEventListener("load", function () {
-        img.src = fileReader.result;
+            document.getElementById(Id).src = fileReader.result;
         }, false);
+
+        document.getElementById(Id).src = fileReader.readAsDataURL(file);
+    }
+}
   
-        img.src = fileReader.readAsDataURL(file);
-     }
-  });
+
+// Formulaires
+document.getElementById('previewfile1').addEventListener('change', function(event){
+    previewImg(event, 'previewimg1');
+});
   
-  var input2 = document.getElementById('previewfile2');
-  var img2 = document.getElementById('previewimg2');
-  
-  input2.addEventListener('change', function(event){
-     var file = event.target.files[0];
-    
-      if(file.size > 500000){
-        alert("Your image is too hight!")
-        return false;
-      }
-     if (file) {
-        var fileReader = new FileReader();
-  
-        fileReader.addEventListener("load", function () {
-           img2.src = fileReader.result;
-        }, false);
-  
-        img2.src = fileReader.readAsDataURL(file);
-     }
-  });
-  
+
+document.getElementById('previewfile2').addEventListener('change', function(event){
+    previewImg(event, 'previewimg2');
+});
